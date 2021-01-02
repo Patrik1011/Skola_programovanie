e313f6c280a95cc2343ba15cd80b23143052c312
Patrik Hoferica <patohoferica1@gmail.com>
2021-01-02 21:41:44 +0100

    skuska...

--- src/polrocnezadanie_studentdatabase/PolrocneZadanie_StudentDatabase.java
+++ src/polrocnezadanie_studentdatabase/PolrocneZadanie_StudentDatabase.java
@@ -15,7 +15,7 @@ public class PolrocneZadanie_StudentDatabase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Zadaj Prve Cislo: ");
     }
     
 }
